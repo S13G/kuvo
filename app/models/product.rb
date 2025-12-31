905cfbc0e215aa486194dec09e9a252a87c53a24
S13G <ayflix0@gmail.com>
2025-12-31 03:17:28 +0100

    added some model functions and rewrote json

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-  has_many :product_categories
+  has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
 
   has_many :product_variants
@@ -14,10 +14,11 @@ class Product < ApplicationRecord
   has_many :product_favorites, dependent: :destroy
   has_many :favorited_by_users, through: :product_favorites, source: :user
 
-  before_create :set_created_by
+  accepts_nested_attributes_for :product_variants, allow_destroy: true
+  accepts_nested_attributes_for :product_images, allow_destroy: true
 
   def self.ransackable_attributes(auth_object = nil)
-    %w[created_by created_at description id is_active name price_cents updated_at]
+    %w[created_by created_at description id is_active name price_cents updated_at ]
   end
 
   def self.ransackable_associations(auth_object = nil)
@@ -35,25 +36,29 @@ class Product < ApplicationRecord
   end
 
   def average_rating
-    product_reviews.average(:rating)&.round(1) || 0
+    @average_rating ||= product_reviews.average(:rating)&.round(1) || 0
+  end
+
+  def total_reviews
+    @total_reviews ||= product_reviews.count
   end
 
   def total_stock
-    product_variants.sum(:stock)
-  end
-
-  def main_image
-    product_images.main.first
+    @total_stock ||= product_variants.sum(:stock)
   end
 
   def favorited_count
     product_favorites.count
   end
 
+  def main_image
+    product_images.find_by(is_main: true)&.image_file
+  end
+
   def discounted_price_cents
     discount = Discount.active.first
     if discount.nil?
-      return price_cents
+      return nil
     end
 
     price_cents - (price_cents * discount.percentage_off / 100)
@@ -64,12 +69,22 @@ class Product < ApplicationRecord
   end
 
   def formatted_discounted_price
-    Money.new(discounted_price_cents, CurrencySetting.currency).format
+    return nil if discounted_price_cents.nil?
+
+    Money.new(discounted_price_cents.present?, CurrencySetting.currency).format
   end
 
-  private
-
-  def set_created_by
-    self.created_by = current_admin_user
+  def as_json(options = nil)
+    {
+      id: id,
+      main_image: main_image,
+      name: name,
+      description: description,
+      price: formatted_price,
+      discounted_price: (formatted_discounted_price if discounted_price_cents.present?),
+      average_rating: @average_rating,
+      total_product_review: @total_reviews,
+      total_stock: @total_stock
+    }
   end
 end
