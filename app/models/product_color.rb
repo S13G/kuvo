63215c8a47cd73688ecbb3e94ba92a3652c8426b
S13G <ayflix0@gmail.com>
2025-12-31 03:17:54 +0100

    added color swatch and display name

--- app/models/product_color.rb
+++ app/models/product_color.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
 
 class ProductColor < ApplicationRecord
-  has_many :product_variant
-  has_many :products, through: :product_variant
+  has_many :product_variants
+  has_many :products, through: :product_variants
 
   validates :name, presence: true
   validates :hex_code,
             format: { with: /\A#(?:\h{3}|\h{6})\z/ },
             allow_nil: true
 
+  def display_name
+    hex_code.present? ? "#{name} (#{hex_code})" : name
+  end
+
+  def swatch(size: 14)
+    return name if hex_code.blank?
+
+    "<span style='
+      display:inline-block;
+      width:#{size}px;
+      height:#{size}px;
+      background:#{hex_code};
+      border-radius:4px;
+      border:1px solid #d1d5db;
+      margin-right:6px;
+      vertical-align:middle;
+    '></span>#{name}".html_safe
+  end
 end
